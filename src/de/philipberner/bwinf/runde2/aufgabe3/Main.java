8aa21c4d3b784140cadd5686bb8965036550793f
termitehuegel <termitehuegel@gmail.com>
2021-12-26 22:34:04 +0100

    generateChangeList

--- src/de/philipberner/bwinf/runde2/aufgabe3/Main.java
+++ src/de/philipberner/bwinf/runde2/aufgabe3/Main.java
@@ -1,8 +1,11 @@
 package de.philipberner.bwinf.runde2.aufgabe3;
 
+import de.philipberner.bwinf.runde2.aufgabe3.siebenSegmentAnzeige.SiebenSegmentAnzeige;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        SiebenSegmentAnzeige siebenSegmentAnzeige = new SiebenSegmentAnzeige();
+        siebenSegmentAnzeige.generateChangeList();
     }
 }
