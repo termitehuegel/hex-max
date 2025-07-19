f67ddf0cd4b94e9ad5b04f86d8248d8e2160f284
Termitehuegel <64739941+termitehuegel@users.noreply.github.com>
2025-07-19 23:02:07 +0200

    renamed getPlaceValues to getDigits

--- src/de/philipberner/bwinf/runde2/aufgabe3/HexNumber.java
+++ src/de/philipberner/bwinf/runde2/aufgabe3/HexNumber.java
@@ -4,24 +4,24 @@ package de.philipberner.bwinf.runde2.aufgabe3;
 public class HexNumber implements Comparable<HexNumber>{
     private final int value;
     private final String hex;
-    private final int[] placeValues;
+    private final int[] digits;
 
     public HexNumber(String hex) {
         this.hex = hex;
-        this.placeValues = calculatePlaceValues();
+        this.digits = calculatePlaceValues();
         this.value = Integer.parseInt(hex, 16);
     }
 
     public HexNumber (int[] values) {
-        this.placeValues = values;
+        this.digits = values;
         this.value = calculateValue();
         this.hex = Integer.toHexString(value).toUpperCase();
     }
 
     private int calculateValue() {
         int val = 0;
-        for (int i=0; i< placeValues.length; i++) {
-            val +=placeValues[placeValues.length-1-i] * Math.pow(16, i);
+        for (int i=0; i< digits.length; i++) {
+            val +=digits[digits.length-1-i] * Math.pow(16, i);
         }
         return val;
     }
@@ -43,8 +43,8 @@ public class HexNumber implements Comparable<HexNumber>{
         return hex;
     }
 
-    public int[] getPlaceValues() {
-        return placeValues;
+    public int[] getDigits() {
+        return digits;
     }
 
     @Override
