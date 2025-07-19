1bbcf1beac9a58a7704622185b54c7eb5f50ea1e
Termitehuegel <64739941+termitehuegel@users.noreply.github.com>
2025-07-19 23:02:08 +0200

    now the moves can be visualized

---
+++ src/de/philipberner/bwinf/runde2/aufgabe3/sevenSegmentDisplay/NumberChanger.java
@@ -0,0 +1,54 @@
+package de.philipberner.bwinf.runde2.aufgabe3.sevenSegmentDisplay;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberChanger {
+    private final SevenSegmentNumber start;
+    private final SevenSegmentNumber finish;
+
+    public NumberChanger(@NotNull SevenSegmentNumber start,  @NotNull SevenSegmentNumber finish) throws Exception {
+        if (finish.getNumber().length != start.getNumber().length) {
+            throw new Exception("finish and start need the same length");
+        }
+        this.start = start;
+        this.finish = finish;
+    }
+
+    public List<SevenSegmentNumber> showSteps(List<int[][]> steps) {
+        List<SevenSegmentNumber> list = new ArrayList<>();
+        for (int[][] step : steps) {
+            start.getNumber()[step[0][0]][step[0][1]] = false;
+            start.getNumber()[step[1][0]][step[1][1]] = true;
+            list.add(new SevenSegmentNumber(start));
+        }
+        return list;
+    }
+
+    public List<int[][]> calculateSteps() throws Exception {
+        List<int[][]> list = new ArrayList<>();
+        List<int[]> toFalse = new ArrayList<>();
+        List<int[]> toTrue = new ArrayList<>();
+        for (int i=0; i<finish.getNumber().length; i++) {
+            for (int j=0; j<finish.getNumber()[i].length; j++) {
+                if (start.getNumber()[i][j] != finish.getNumber()[i][j]) {
+                    if (start.getNumber()[i][j]) {
+                        toFalse.add(new int[] {i, j});
+                    } else {
+                        toTrue.add(new int[]{i, j});
+                    }
+                }
+            }
+        }
+        if (toFalse.size() != toTrue.size()) {
+            throw new Exception("Numbers need to have the same number of positions");
+        }
+        for (int i=0; i<toFalse.size(); i++) {
+            list.add(new int[][]{toFalse.get(i), toTrue.get(i)});
+        }
+
+        return list;
+    }
+}
